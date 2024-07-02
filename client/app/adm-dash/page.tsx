5ecd3ca10bc6b3def62fc3e021e7bc0de2bddec8
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-07-02 18:14:05 +0530

    Done with full employee dashboard

--- client/app/adm-dash/page.tsx
+++ client/app/adm-dash/page.tsx
@@ -69,8 +69,8 @@ const Page = () => {
                                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                              fill="none"
                                              viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                  stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                            <path stroke="currentColor" strokeLinecap={"round"} strokeLinejoin={"round"}
+                                                  strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                         </svg>
                                     </button>
 
@@ -240,9 +240,8 @@ const Page = () => {
                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 11 20">
-                                            <path stroke="currentColor" stroke-linecap="round"
-                                                  stroke-linejoin="round"
-                                                  stroke-width="2"
+                                            <path stroke="currentColor" strokeLinecap={"round"} strokeLinejoin={"round"}
+                                                  strokeWidth={2}
                                                   d="M1.75 15.363a4.954 4.954 0 0 0 2.638 1.574c2.345.572 4.653-.434 5.155-2.247.502-1.813-1.313-3.79-3.657-4.364-2.344-.574-4.16-2.551-3.658-4.364.502-1.813 2.81-2.818 5.155-2.246A4.97 4.97 0 0 1 10 5.264M6 17.097v1.82m0-17.5v2.138"/>
                                         </svg>
                                         <div
