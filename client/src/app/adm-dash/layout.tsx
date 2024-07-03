9ed8070519d75e1d75c6986939b0783ccc0679e5
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-07-03 19:39:55 +0530

    Created not authorized page

--- client/src/app/adm-dash/layout.tsx
+++ client/src/app/adm-dash/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Page from "@/app/components/page";
 
-const Admin = ({children}) => {
+const Admin = ({children}:any) => {
     return (
         <>
             <Page/>
--- client/src/app/mng-dash/page.tsx
+++ client/src/app/mng-dash/page.tsx
@@ -19,7 +19,8 @@ function Manager() {
                             <div className="flex justify-start items-center">
                                 <button aria-expanded="true" aria-controls="sidebar"
                                         className="hidden p-2 mr-3 text-gray-600 rounded cursor-pointer lg:inline hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
-                                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                         fill="none"
                                          viewBox="0 0 16 12">
                                         <path stroke="currentColor" strokeLinecap={"round"} strokeLinejoin={"round"}
                                               strokeWidth={2} d="M1 1h14M1 6h14M1 11h7"/>
@@ -37,7 +38,8 @@ function Manager() {
                                         className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                                     <span className="sr-only">Search</span>
 
-                                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                         fill="none"
                                          viewBox="0 0 20 20">
                                         <path stroke="currentColor" strokeLinecap={"round"} strokeLinejoin={"round"}
                                               strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -68,71 +70,85 @@ function Manager() {
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 18 20">
                                                 <path
                                                     d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Sales</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Sales
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 20 19">
                                                 <path
                                                     d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"/>
                                                 <path
                                                     d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Users</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Users
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 18 18">
                                                 <path
                                                     d="M15.977.783A1 1 0 0 0 15 0H3a1 1 0 0 0-.977.783L.2 9h4.239a2.99 2.99 0 0 1 2.742 1.8 1.977 1.977 0 0 0 3.638 0A2.99 2.99 0 0 1 13.561 9H17.8L15.977.783ZM6 2h6a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2Zm7 5H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Z"/>
                                                 <path
                                                     d="M1 18h16a1 1 0 0 0 1-1v-6h-4.439a.99.99 0 0 0-.908.6 3.978 3.978 0 0 1-7.306 0 .99.99 0 0 0-.908-.6H0v6a1 1 0 0 0 1 1Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Inbox</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Inbox
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 20 20">
                                                 <path
                                                     d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Profile</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Profile
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 20 20">
                                                 <path
                                                     d="M18 7.5h-.423l-.452-1.09.3-.3a1.5 1.5 0 0 0 0-2.121L16.01 2.575a1.5 1.5 0 0 0-2.121 0l-.3.3-1.089-.452V2A1.5 1.5 0 0 0 11 .5H9A1.5 1.5 0 0 0 7.5 2v.423l-1.09.452-.3-.3a1.5 1.5 0 0 0-2.121 0L2.576 3.99a1.5 1.5 0 0 0 0 2.121l.3.3L2.423 7.5H2A1.5 1.5 0 0 0 .5 9v2A1.5 1.5 0 0 0 2 12.5h.423l.452 1.09-.3.3a1.5 1.5 0 0 0 0 2.121l1.415 1.413a1.5 1.5 0 0 0 2.121 0l.3-.3 1.09.452V18A1.5 1.5 0 0 0 9 19.5h2a1.5 1.5 0 0 0 1.5-1.5v-.423l1.09-.452.3.3a1.5 1.5 0 0 0 2.121 0l1.415-1.414a1.5 1.5 0 0 0 0-2.121l-.3-.3.452-1.09H18a1.5 1.5 0 0 0 1.5-1.5V9A1.5 1.5 0 0 0 18 7.5Zm-8 6a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Settings</div>
+                                            <div
+                                                className="text-sm font-medium text-gray-900 dark:text-white">Settings
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 20 16">
                                                 <path
                                                     d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Products</div>
+                                            <div
+                                                className="text-sm font-medium text-gray-900 dark:text-white">Products
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
@@ -140,17 +156,20 @@ function Manager() {
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
                                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                                 viewBox="0 0 11 20">
-                                                <path stroke="currentColor" strokeLinecap={"round"} strokeLinejoin={"round"}
+                                                <path stroke="currentColor" strokeLinecap={"round"}
+                                                      strokeLinejoin={"round"}
                                                       strokeWidth={2}
                                                       d="M1.75 15.363a4.954 4.954 0 0 0 2.638 1.574c2.345.572 4.653-.434 5.155-2.247.502-1.813-1.313-3.79-3.657-4.364-2.344-.574-4.16-2.551-3.658-4.364.502-1.813 2.81-2.818 5.155-2.246A4.97 4.97 0 0 1 10 5.264M6 17.097v1.82m0-17.5v2.138"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Pricing</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Pricing
+                                            </div>
                                         </a>
                                         <a href="#"
                                            className="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
                                             <svg
                                                 className="mx-auto mb-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
                                                 viewBox="0 0 16 20">
                                                 <path
                                                     d="M7 11H5v1h2v-1Zm4 3H9v1h2v-1Zm-4 0H5v1h2v-1ZM5 5V.13a2.98 2.98 0 0 0-1.293.749L.88 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
@@ -158,7 +177,8 @@ function Manager() {
                                                     d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM13 16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v6Zm-1-8H9a1 1 0 0 1 0-2h3a1 1 0 1 1 0 2Zm0-3H9a1 1 0 0 1 0-2h3a1 1 0 1 1 0 2Z"/>
                                                 <path d="M11 11H9v1h2v-1Z"/>
                                             </svg>
-                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Billing</div>
+                                            <div className="text-sm font-medium text-gray-900 dark:text-white">Billing
+                                            </div>
                                         </a>
 
                                     </div>
@@ -168,7 +188,8 @@ function Manager() {
                                         id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
                                     <span className="sr-only">Open user menu</span>
                                     <img className="w-8 h-8 rounded-full"
-                                         src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
+                                         src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                         alt="user photo"/>
                                 </button>
 
                                 <div
@@ -205,7 +226,50 @@ function Manager() {
                         </div>
                     </nav>
                 </header>
-            ):(<p>Not authorized</p>)}
+            ) : (<>
+                <div className="h-[20rem] w-[40rem] bg-gray-200 mx-auto text-center m-4 rounded-xl py-12">
+                    <h2 className={"flex items-center justify-center text-2xl p-3 font-bold text-red-600"}>
+                        <svg
+                            className={"mx-2"}
+                            fill="#e31616" height="30px" width="30px" version="1.1" id="Layer_1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512" stroke="#e31616">
+                            <g id="SVGRepo_bgCarrier" strokeWidth={0}></g>
+                            <g id="SVGRepo_tracerCarrier" strokeLinecap={"round"} strokeLinejoin={"round"}></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M505.403,406.394L295.389,58.102c-8.274-13.721-23.367-22.245-39.39-22.245c-16.023,0-31.116,8.524-39.391,22.246 L6.595,406.394c-8.551,14.182-8.804,31.95-0.661,46.37c8.145,14.42,23.491,23.378,40.051,23.378h420.028 c16.56,0,31.907-8.958,40.052-23.379C514.208,438.342,513.955,420.574,505.403,406.394z M477.039,436.372 c-2.242,3.969-6.467,6.436-11.026,6.436H45.985c-4.559,0-8.784-2.466-11.025-6.435c-2.242-3.97-2.172-8.862,0.181-12.765 L245.156,75.316c2.278-3.777,6.433-6.124,10.844-6.124c4.41,0,8.565,2.347,10.843,6.124l210.013,348.292 C479.211,427.512,479.281,432.403,477.039,436.372z"></path>
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M256.154,173.005c-12.68,0-22.576,6.804-22.576,18.866c0,36.802,4.329,89.686,4.329,126.489 c0.001,9.587,8.352,13.607,18.248,13.607c7.422,0,17.937-4.02,17.937-13.607c0-36.802,4.329-89.686,4.329-126.489 C278.421,179.81,268.216,173.005,256.154,173.005z"></path>
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M256.465,353.306c-13.607,0-23.814,10.824-23.814,23.814c0,12.68,10.206,23.814,23.814,23.814 c12.68,0,23.505-11.134,23.505-23.814C279.97,364.13,269.144,353.306,256.465,353.306z"></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                        You are not Authorized
+                    </h2>
+                    <div>Click this below button to login or else contact your administrator</div>
+                    <div className={"m-5"}>
+                        <a href="/">
+                            <button type="submit"
+                                    className={"bg-blue-500 m-5 p-3 rounded-lg text-white font-semibold focus:ring-blue-300 focus:ring-4 hover:bg-blue-700 duration-300"}>Go
+                                to Login
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </>)}
         </div>
     );
 }
