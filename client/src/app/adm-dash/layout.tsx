8b29c3f3a1c9f429bbb6ea6df42f6e936145d576
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-07-09 09:04:47 +0530

    Testing of the logic remaining but implementation completed

--- client/src/app/adm-dash/layout.tsx
+++ client/src/app/adm-dash/layout.tsx
@@ -3,8 +3,13 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import Header from "@/app/components/Header";
 import Sidebar from "@/app/components/Sidebar";
-import Page from "@/app/adm-dash/page";
 import jsonwebtoken from "jsonwebtoken";
+
+// type tokenInfo={
+//     exp:number
+//     iat:number
+//     sub:string
+// }
 
 type AdminData = {
   token: string;
@@ -22,26 +27,37 @@ const Admin: React.FC<AdminProps> = ({ children }) => {
     adminDetails: {},
     loginTimeDetails: {},
   });
-
-  useEffect(() => {
-    const user = localStorage.getItem("user");
-    const is_expired = jsonwebtoken.decode(JSON.parse(user!).token);
-    console.log(is_expired);
-    if (jsonwebtoken.decode(JSON.parse(user!).token)!.exp < Date.now() / 1000) {
-      const parsedUser = JSON.parse(user);
+  const user = localStorage.getItem("user");
+  const grabToken = () => {
+    if (user) {
+      const parsedUser = JSON.parse(user!);
       setData({
         token: parsedUser.token,
         adminDetails: parsedUser.adminDetails,
         loginTimeDetails: parsedUser.loginTimeDetails,
       });
     }
+  };
+
+  useEffect(() => {
+    grabToken();
+    setInterval(() => {
+      const is_expired = jsonwebtoken.decode(JSON.parse(user!).token);
+      if (
+        typeof is_expired === "object" &&
+        is_expired !== null &&
+        "exp" in is_expired &&
+        typeof is_expired.exp === "number"
+      ) {
+        console.log(is_expired);
+      }
+    }, 1000);
   }, []);
 
   return (
     <>
       <Header />
       <Sidebar />
-      <Page data={data} />
       <main>{children}</main>
     </>
   );
