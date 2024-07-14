03658a4fe8588bbb3aceb97e3413f5ae133b0a44
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-07-14 18:36:24 +0530

    Done with pagination at Frontend

--- client/src/app/adm-dash/get-emp/page.tsx
+++ client/src/app/adm-dash/get-emp/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
 import {
   Dialog,
   DialogBackdrop,
@@ -10,27 +9,50 @@ import {
 } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function EmployeeDetails() {
   const token = localStorage.getItem("user");
   const parseToken = JSON.parse(token!);
   const [empData, setEmpData] = useState<any>([]);
-  const getEmployeeData = async () => {
-    if (parseToken) {
-      const response = await axios.get("http://localhost:8080/add-employee", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${parseToken.token}`,
-        },
-      });
-      const data: AxiosResponse = await response.data;
-      setEmpData(data);
-      console.log(data);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const itemsPerPage = 5;
+  const getEmployeeData = async (page: number) => {
+    try {
+      if (parseToken) {
+        const response = await axios.get(
+          `http://localhost:8080/add-employee?page=${page}&size=${itemsPerPage}`,
+          {
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${parseToken.token}`,
+            },
+          },
+        );
+        const data = await response.data;
+        setEmpData(data.content);
+        setTotalPages(data.totalPages);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
+  const handlePageChange = (page: any) => {
+    setCurrentPage(page);
+  };
   useEffect(() => {
-    getEmployeeData();
-  }, []);
+    getEmployeeData(currentPage);
+  }, [currentPage]);
 
   const [open, setOpen] = React.useState<boolean>(true);
   const navigate = useRouter();
@@ -117,96 +139,36 @@ export default function EmployeeDetails() {
                 </div>
               </li>
             ))}
-            <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-              <div className="flex flex-1 justify-between sm:hidden">
-                <a
-                  href="#"
-                  className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                >
-                  Previous
-                </a>
-                <a
-                  href="#"
-                  className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                >
-                  Next
-                </a>
-              </div>
-              <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                <div>
-                  <p className="text-sm text-gray-700">
-                    Showing <span className="font-medium">1</span> to{" "}
-                    <span className="font-medium">10</span> of{" "}
-                    <span className="font-medium">97</span> results
-                  </p>
-                </div>
-                <div>
-                  <nav
-                    aria-label="Pagination"
-                    className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-                  >
-                    <a
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    className={`${currentPage === 0 ? "cursor-not-allowed" : ""}`}
+                  />
+                </PaginationItem>
+                {[...Array(totalPages)].map((_, index) => (
+                  <PaginationItem key={index}>
+                    <PaginationLink
                       href="#"
-                      className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      isActive={index === currentPage}
+                      onClick={() => handlePageChange(index)}
                     >
-                      <span className="sr-only">Previous</span>
-                      <ChevronLeftIcon aria-hidden="true" className="h-5 w-5" />
-                    </a>
-                    {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-                    <a
-                      href="#"
-                      aria-current="page"
-                      className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      1
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                    >
-                      2
-                    </a>
-                    <a
-                      href="#"
-                      className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                    >
-                      3
-                    </a>
-                    <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                      ...
-                    </span>
-                    <a
-                      href="#"
-                      className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                    >
-                      8
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                    >
-                      9
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                    >
-                      10
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                    >
-                      <span className="sr-only">Next</span>
-                      <ChevronRightIcon
-                        aria-hidden="true"
-                        className="h-5 w-5"
-                      />
-                    </a>
-                  </nav>
-                </div>
-              </div>
-            </div>
+                      {index + 1}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))}
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    className={`${currentPage === totalPages - 1 ? "cursor-not-allowed" : ""}`}
+                  />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
           </ul>
         </div>
       ) : (
